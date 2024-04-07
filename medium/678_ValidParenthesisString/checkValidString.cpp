280c00f32b3f31a2cf10ca5353386c681c45c00a
hungry5656 <hhhhhenrysun@gmail.com>
2024-04-07 13:34:41 -0700

    finished problem 678

---
+++ medium/678_ValidParenthesisString/checkValidString.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool checkValidString(string s) {
+        int leftMin = 0, leftMax = 0;
+        for (auto &ch:s) {
+            if (ch == '(') {
+                leftMin++;
+                leftMax++;
+            } else if (ch == ')') {
+                leftMin--;
+                leftMax--;
+            } else {
+                leftMin--;
+                leftMax++;
+            }
+            if (leftMin < 0) {
+                leftMin = 0;
+            }
+            if (leftMax < 0) {
+                return false;
+            }
+        }
+        return leftMin == 0;
+        
+    }
+};
