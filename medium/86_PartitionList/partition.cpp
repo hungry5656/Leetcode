554790ded29d3cfa787c66e13a65e3d9d622e957
hungry5656 <hhhhhenrysun@gmail.com>
2024-04-10 20:34:35 -0700

    finished problem 86

---
+++ medium/86_PartitionList/partition.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* partition(ListNode* head, int x) {
+        ListNode *leftHead = new ListNode(0), *rightHead = new ListNode(0);
+        ListNode *left = leftHead, *right = rightHead;
+        while(head != nullptr) {
+            if (head->val < x) {
+                left->next = head;
+                left = left->next;
+            } else {
+                right->next = head;
+                right = right->next;
+            }
+            head = head->next;
+        }
+        left->next = rightHead->next;
+        right->next = nullptr;
+        
+        return leftHead->next;
+    }
+};
