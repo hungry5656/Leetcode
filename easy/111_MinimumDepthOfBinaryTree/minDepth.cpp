c5f705177dfe033fef88d78ce7e1bbe9ae590b9c
hungry5656 <hhhhhenrysun@gmail.com>
2024-04-18 23:15:43 -0700

    finished problem 111

---
+++ easy/111_MinimumDepthOfBinaryTree/minDepth.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int minDepth(TreeNode* root) {
+        if (!root) {
+            return 0;
+        }
+        queue<pair<TreeNode*, int>> nodeQ;
+        nodeQ.push({root, 1});
+        while (!nodeQ.empty()) {
+            pair<TreeNode*, int> currNode = nodeQ.front();
+            nodeQ.pop();
+            if (currNode.first->left == nullptr && currNode.first->right == nullptr) {
+                return currNode.second;
+            }
+            if (currNode.first->left) {
+                nodeQ.push({currNode.first->left, currNode.second + 1});
+            }
+            if (currNode.first->right) {
+                nodeQ.push({currNode.first->right, currNode.second + 1});
+            }
+        }
+        return 0;
+    }
+};
