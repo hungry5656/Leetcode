6fea60bd87ba01149d3c13b38c2c06bdea2b87ad
hungry5656 <hhhhhenrysun@gmail.com>
2024-09-09 15:41:14 -0700

    finished problem 230

---
+++ medium/230_KthSmallestElementInABST/kthSmallest.cpp
@@ -0,0 +1,57 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int retVal;
+        kthSmallestHelper(root, pq);
+        for (int i = 0; i < k; ++i) {
+            retVal = pq.top();
+            pq.pop();
+        }
+        return retVal;
+    }
+    void kthSmallestHelper(TreeNode* root, priority_queue<int, vector<int>, greater<int>> &pq, int k) {
+        if (!root)
+            return;
+        pq.push(root->val);
+        if (pq.size() > k) {
+            pop
+        }
+        kthSmallestHelper(root->left, pq);
+        kthSmallestHelper(root->right, pq);
+    }
+};
+
+/* original solution:
+class Solution {
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        priority_queue<int, vector<int>, greater<int>> pq;
+        int retVal;
+        kthSmallestHelper(root, pq);
+        for (int i = 0; i < k; ++i) {
+            retVal = pq.top();
+            pq.pop();
+        }
+        return retVal;
+    }
+    void kthSmallestHelper(TreeNode* root, priority_queue<int, vector<int>, greater<int>> &pq) {
+        if (!root)
+            return;
+        pq.push(root->val);
+        kthSmallestHelper(root->left, pq);
+        kthSmallestHelper(root->right, pq);
+    }
+};
+*/
