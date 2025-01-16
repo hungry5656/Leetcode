5266d183ff76a3ec9833b143f6eed3cdedbb9980
hungry5656 <hhhhhenrysun@gmail.com>
2025-01-15 23:27:36 -0800

    finish problem 80

---
+++ medium/80_RemoveDuplicatesFromSortedArrayII/removeDuplicates.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int k = 1, cnt = 1;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] != nums[i - 1]) {
+                cnt = 1;
+                nums[k] = nums[i];
+                k++;
+            } else {
+                cnt++;
+                if (cnt <= 2) {
+                    nums[k] = nums[i];
+                    k++;
+                }
+            }
+        }
+        return k;
+    }
+};
