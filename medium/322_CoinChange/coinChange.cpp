076f0ec3f74139974e53be3ec0cbfc75aad50d44
Sizhuo Sun <hhhhhenrysun@gmail.com>
2025-10-06 20:00:30 -0700

    optimized problem 322

--- medium/322_CoinChange/coinChange.cpp
+++ medium/322_CoinChange/coinChange.cpp
@@ -1,6 +1,28 @@
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
+        vector<int> mp(amount + 1, -1);
+        mp[0] = 0;
+        for (int i = 1; i <= amount; ++i) {
+            int currMin = INT_MAX;
+            for (int j = 0; j < coins.size(); ++j) {
+                int currCoin = coins[j];
+                if (currCoin <= i && i - currCoin < amount && mp[i - currCoin] != -1) {
+                    currMin = min(currMin, 1 + mp[i - currCoin]);
+                }
+            }
+            if (currMin != INT_MAX) {
+                mp[i] = currMin;
+            }
+        }
+        return mp[amount];
+    }
+};
+
+/* original solution
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
         if (amount == 0) return 0;
         vector<int> dp(amount + 1, -1);
         dp[0] = 0;
@@ -21,3 +43,4 @@ public:
         return dp[amount];
     }
 };
+*/
