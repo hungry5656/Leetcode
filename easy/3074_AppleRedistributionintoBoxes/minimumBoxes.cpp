6e91c6752062ee15d2ea4cb12744a47d3d745473
Sizhuo Sun <hhhhhenrysun@gmail.com>
2025-12-24 20:55:43 -0500

    finished problem 3074

---
+++ easy/3074_AppleRedistributionintoBoxes/minimumBoxes.cpp
@@ -0,0 +1,19 @@
+#include <functional>
+class Solution {
+public:
+    int minimumBoxes(vector<int>& apple, vector<int>& capacity) {
+        int sum = 0;
+        for (int i = 0; i < apple.size(); ++i) {
+            sum += apple[i];
+        }
+        sort(capacity.begin(), capacity.end(), greater<int>());
+        int cnt = 0;
+        for (int i = 0; i < capacity.size(); ++i) {
+            if (sum <= 0)
+                break;
+            sum -= capacity[i];
+            cnt++;
+        }
+        return cnt;
+    }
+};
